a1fc540c96d2107a309a329f099224b30f77bd04
kameel4 <kamilshaimiev@yandex.ru>
2025-04-21 23:09:29 +0300

    developed dijkstra with finding all shortest paths for betweenness centrality (who could know there are more than 1 shortest path between 2 nodes) and betweenness centrality itself. An example of CLOSURE (dfs) in get_all_paths method

--- practicum_3/homework/centrality_measures.py
+++ practicum_3/homework/centrality_measures.py
@@ -7,7 +7,7 @@ import networkx as nx
 from src.plotting.graphs import plot_graph, plot_network_via_plotly
 from src.common import AnyNxGraph
 
-from dijkstra import dijkstra
+from dijkstra import dijkstra, dijkstra_all_paths, get_all_paths
 
 
 class CentralityMeasure(Protocol):
@@ -16,23 +16,38 @@ class CentralityMeasure(Protocol):
 
 
 def closeness_centrality(G: AnyNxGraph) -> dict[Any, float]:
-    node_centrality = dict()
+    node_closeness_centrality = dict()
     n = len(G)
     for node in range(n):
         dist_array = dijkstra(G, node)
-        node_centrality[node] = (1 / sum([dist_array[i] for i in range(n)]))
-    return node_centrality
+        node_closeness_centrality[node] = (1 / sum([dist_array[i] for i in range(n)]))
+    return node_closeness_centrality
 
 
 
 def betweenness_centrality(G: AnyNxGraph) -> dict[Any, float]: 
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    #########################
-
-    pass
-
+    node_betweenness_centrality = dict()
+    all_paths = dict()
+    n = len(G)
+    for start_node in range(n):
+        distances, predcessors = dijkstra_all_paths(G, start_node)
+        all_paths_from_node = [get_all_paths(predcessors, start_node, end_node) for end_node in range(n)]
+        all_paths[start_node] = all_paths_from_node
+    
+    for bridge_node in range(n):
+        sigma = 0
+        for s in range(n):
+            for t in range(s+1, n):
+                if (bridge_node != s and bridge_node != t):
+                    shortest_paths, shortest_paths_with_bridge = 0, 0
+                    for path in all_paths[s][t]:
+                        if bridge_node in path:
+                            shortest_paths_with_bridge +=1
+                        shortest_paths +=1
+                    sigma += shortest_paths_with_bridge / shortest_paths
+        node_betweenness_centrality[bridge_node] = sigma
+    return node_betweenness_centrality
+    
 
 def eigenvector_centrality(G: AnyNxGraph) -> dict[Any, float]: 
 
@@ -55,6 +70,6 @@ if __name__ == "__main__":
     G = nx.karate_club_graph()
 
     plot_centrality_measure(G, closeness_centrality)
-    # plot_centrality_measure(G, betweenness_centrality)
+    plot_centrality_measure(G, betweenness_centrality)
     # plot_centrality_measure(G, eigenvector_centrality)
 
--- practicum_3/homework/dijkstra.py
+++ practicum_3/homework/dijkstra.py
@@ -1,12 +1,13 @@
 from typing import Any, Protocol
 from itertools import combinations
 import heapq
+from collections import defaultdict
 
 import numpy as np
 import networkx as nx
 
 from src.plotting.graphs import plot_graph, plot_network_via_plotly
-from src.common import AnyNxGraph 
+from src.common import AnyNxGraph
 
 def dijkstra(graph: AnyNxGraph, source: Any) -> dict[Any, float]:
     distance = {node: float('inf') for node in graph.nodes}
@@ -32,6 +33,37 @@ def dijkstra(graph: AnyNxGraph, source: Any) -> dict[Any, float]:
 
     return distance
 
+def dijkstra_all_paths(graph, start):
+    distances = {node: float('inf') for node in graph.nodes()}
+    distances[start] = 0
+    predecessors = defaultdict(list)
+    heap = [(0, start)]
+    
+    while heap:
+        current_dist, u = heapq.heappop(heap)
+        if current_dist > distances[u]:
+            continue
+        for v in graph.neighbors(u):
+            new_dist = current_dist + 1
+            if new_dist < distances[v]:
+                distances[v] = new_dist
+                predecessors[v] = [u]
+                heapq.heappush(heap, (new_dist, v))
+            elif new_dist == distances[v]:
+                predecessors[v].append(u)
+    return distances, predecessors
+
+def get_all_paths(predecessors, start, end):
+    paths = []
+    def dfs(node, path):
+        if node == start:
+            paths.append(list(reversed(path)))
+            return
+        for pred in predecessors[node]:
+            dfs(pred, path + [pred])
+    dfs(end, [end])
+    return paths
+
 
 G_karate = nx.karate_club_graph()
 
@@ -41,5 +73,12 @@ G_karate = nx.karate_club_graph()
 # print("\n\n\n")
 
 # print(list(nx.all_pairs_dijkstra(G_karate))[0])
+# print(list(nx.all_shortest_paths(G_karate, 0, 33)))
 
-# print(dijkstra(G_karate, 0))
+all_paths = dict()
+for start_node in range(0, 34):
+    distances, predcessors = dijkstra_all_paths(G_karate, start_node)
+    all_paths_from_node = [get_all_paths(predcessors, start_node, end_node) for end_node in range(34)]
+    all_paths[start_node] = all_paths_from_node
+
+print(all_paths[0][33])
